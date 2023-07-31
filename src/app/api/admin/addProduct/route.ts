37bc1f776f109f6a19896ee2ad2d962ee8200d6e
Your Name <you@example.com>
2023-07-31 11:53:35 -0500

    update api

--- src/app/api/admin/addProduct/route.ts
+++ src/app/api/admin/addProduct/route.ts
@@ -1,3 +1,37 @@
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { options } from "../../auth/[...nextauth]/options";
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const session = await getServerSession(req, res, options)
 
-export async function GET(request: Request) {}
+  if (!session) {
+    res.status(401).json({ message: "You must be logged in." });
+    return;
+  }
+  if (session.user.role === "ADMIN") {
+    try {
+        
+    } catch (error: any) {
+        return new NextResponse(
+            JSON.stringify({
+                status: "error",
+                message: error.message,
+            }), {status: 500}
+        )
+        
+    }
+    return res.json({
+        message: "es admin"
+    })
+  }
+
+  return NextResponse.json({
+    status: "error",
+    messsage: "no eres admin",
+}, {status: 401})
+
+  
+}
