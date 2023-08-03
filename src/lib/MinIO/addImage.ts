6537cebf81dcba45b900a1905a8a1ba70dae8940
Your Name <you@example.com>
2023-08-03 16:26:37 -0500

    return cleaning

--- src/lib/MinIO/addImage.ts
+++ src/lib/MinIO/addImage.ts
@@ -9,21 +9,22 @@ export default async function addImage(title: String, image: File): Promise<stri
         secretKey: 'C4bSiQFQgYqrwWjYwqe4vDprdGwPmfF8jgEmxbF1'
     });
     if (image) {
-    const imageName = image.name;
-    const fileKey: string = `images/${Date.now()}_${imageName}`;
-    const bytes = await image.arrayBuffer();
-    const imgBuffer = Buffer.from(bytes);
-    minioClient.putObject('productos', fileKey, imgBuffer, function (error, objInfo) {
+        const imageName = image.name;
+        const fileKey: string = `images/${Date.now()}_${imageName}`;
+        const bytes = await image.arrayBuffer();
+        const imgBuffer = Buffer.from(bytes);
+        minioClient.putObject('productos', fileKey, imgBuffer, function (error, objInfo) {
             if (error) {
                 console.log(error);
                 return error;
             }
             console.log("EXITO", objInfo);
-            console.log("agregar objectName",fileKey)
-            return fileKey;
+            console.log("agregar objectName", fileKey)
         });
+        return fileKey
+
+    } else {
+        return "no hay imagen"
     }
-    // Using fPutObject API upload your file to the bucket europetrip.
-    return "no lo se"
 
 }
